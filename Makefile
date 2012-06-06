19d8ee9232f6237b472c55df7eaa6fbc69793047
Sadique Ali <sadiqalikm@gmail.com>
2012-06-07 00:41:08 +0530

    Added Makefile and example.rb

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Example Makefile
+
+foo:
+	@echo Foo target
+foo:
+	@echo Redefined foo target	
---
+++ example.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#example.rb
+
+def example
+  puts "example " * 10
+end
+
+def example
+  puts "foobar " * 10
+end
+
+example
